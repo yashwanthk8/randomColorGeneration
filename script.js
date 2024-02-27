e916ab58a8d216428675e0691ad10eee45b0d8ac
yashwanthk8 <101146133+yashwanthk8@users.noreply.github.com>
2024-02-27 23:53:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+     
+console.log("js initializing")
+function check(){
+function rand(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+let ele = document.querySelector(".container").children 
+Array.from(ele).forEach(e=>{
+    e.style.backgroundColor = rand()
+    e.style.color = rand()
+})
+}
+function navi(){
+    window.location.href='index.html';
+}
